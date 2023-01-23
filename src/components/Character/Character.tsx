f467063433e7ea15e8e316d127a7131532e23159
LLpanov <leonardopanov@yahoo.com>
2023-01-23 05:04:09 +0200

    Rick && Morty && TS

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from 'react';
 
 import { ICharacter } from '../../interfaces';
+import styles from './Character.module.scss';
 
 interface ICharacterProps {
 	character: ICharacter;
 }
 
-const Character: FC<ICharacterProps> = ({ character: { name, gender, image } }) => {
+const Character: FC<ICharacterProps> = ({ character: { name, gender, image, status } }) => {
 	return (
 		<>
-			<div>
-				<div>Name : {name}</div>
-				<div>Gender : {gender}</div>
-				<img src={image} alt={name} />
+			<div className={styles.characterCard}>
+				<div className={styles.cardContainer}>
+					<img src={image} alt={name} />
+					<div className={status === 'Alive' ? styles.statusAlive : styles.statusDead}>{status}</div>
+					<div className={styles.cardContent}>
+						<h2>{name}</h2>
+						<p>Gender : {gender}</p>
+					</div>
+				</div>
 			</div>
 		</>
 	);
--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -24,20 +24,20 @@ const Characters: FC = () => {
 					<div className='relative'>
 						<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
 							<svg aria-hidden='true' className='w-5 h-5 text-gray-500 dark:text-gray-400' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-								<path stroke-linejoin='round' stroke-width='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
+								<path strokeLinejoin='round' strokeWidth='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
 							</svg>
 						</div>
 						<input
 							type='search'
 							id='default-search'
 							className={
-								'dark:placeholder-gray-400dark:text-white block w-[550px] rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-500 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-600 dark:focus:border-blue-500 dark:focus:ring-blue-500;'
+								'dark:placeholder-gray-100 dark:text-white block w-[550px] rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-500 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-600 dark:focus:border-blue-500 dark:focus:ring-blue-500;'
 							}
 							placeholder='Search characters...'
 						/>
 						<button
 							type='submit'
-							className='text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+							className='text-white absolute right-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
 						>
 							Search
 						</button>
@@ -45,7 +45,7 @@ const Characters: FC = () => {
 				</form>
 			</div>
 
-			<div className={'flex flex-wrap flex-col mt-4'}>
+			<div className={'grid grid-cols-2 gap-4 mt-10 place-items-center '}>
 				{characters.map(character => (
 					<Character key={character.id} character={character} />
 				))}
