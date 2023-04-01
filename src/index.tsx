590e128a2ebd2e55ab0214a6430bb00e6d1c690d
LLpanov <leonardopanov@yahoo.com>
2023-04-01 18:51:33 +0300

    ui

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
-import './index.css';
+import './index.scss';
 import { setupStore } from './store';
 
 const store = setupStore();
